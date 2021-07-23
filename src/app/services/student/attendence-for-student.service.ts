78c43155afae72332d259f79185998a2c09d7d7a
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-07-24 03:47:21 +0600

    Student attendence saving

--- src/app/services/student/attendence-for-student.service.ts
+++ src/app/services/student/attendence-for-student.service.ts
@@ -19,5 +19,21 @@ export class AttendenceForStudentService {
     return this.http.get<any>(`http://localhost:52684/api/attendances/current/${user.data.id}` , header)
   }
 
+  public saveAttendenceRecord(attendancesId) {
+    var user = JSON.parse( localStorage.getItem('isLoggedIn'));
+   
+    var header = {
+        headers: new HttpHeaders()
+          .set('Authorization',  `Bearer ${user.token}`)
+    }
+      
+    var body = {
+      "attendancesId": attendancesId,
+      "GivenTime": new Date(),
+      "StudentId": user.data.id,
+    }
+    return this.http.post<any>("http://localhost:52684/api/records",body,header)
+  }
+
 
 }
