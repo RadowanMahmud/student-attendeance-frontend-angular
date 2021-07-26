36e5a37516fdf95bf0c063af913437fe22d3dab1
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-07-27 03:17:09 +0600

    STudent can edit student info

--- src/app/student-pages/student-profile-edit/student-profile-edit.component.ts
+++ src/app/student-pages/student-profile-edit/student-profile-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AttendenceForStudentService } from 'app/services/student/attendence-for-student.service';
 
 @Component({
   selector: 'app-student-profile-edit',
@@ -15,8 +16,11 @@ export class StudentProfileEditComponent implements OnInit {
   user_email: string='';
   user_password: string='';
   user_confirm_password: string='';
+  user_new_password: string='';
+  user_id=0;
 
-  constructor(private router:Router) { }
+
+  constructor(public service: AttendenceForStudentService,private router:Router) { }
 
   ngOnInit(): void {
     var user = JSON.parse(localStorage.getItem("isLoggedIn"));
@@ -26,8 +30,50 @@ export class StudentProfileEditComponent implements OnInit {
     this.user_email = user.data.email;
     this.user_session = user.data.session;
     this.user_password = user.data.password;
+    this.user_id = user.data.id
   }
 
-
+  saveEditedInfo(): void{
+    console.log('calling');
+    if(this.user_new_password.length > 0){
+      if(this.user_confirm_password != this.user_password){
+        alert('Password and Confirm Password do not match');
+        return;
+      }
+      else{
+        if(this.user_new_password.length > 6){
+            this.user_password = this.user_new_password;
+        }else{
+          alert("Password must contain at least 6 characters");
+          return;
+        }
+      }
+    }
+    var body = {
+      "Id": this.user_id,
+      "Name": this.user_name,
+      "Roll": this.user_roll,
+      "Phone": this.user_phone,
+      "Session": this.user_session,
+      "Email": this.user_email,
+      "Password": this.user_password,
+      "Roles": "student",
+    }
+    this.service.saveStudentEditedInfo(body).subscribe(
+      (response: any) => {
+          if (response) {
+             alert("Student Informations changed");
+             localStorage.removeItem('isLoggedIn');
+             this.router.navigateByUrl('login');
+          }
+          else {
+              console.log("not succeed");
+          }
+      },
+      error => {                             
+          alert('Please Try Again');
+      }
+    );
+  }
 
 }
