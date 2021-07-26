9a086dcdefc8b60d543d7a3a476da9ce0e075937
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-07-26 20:06:51 +0600

    User page ui added

--- src/app/student-pages/student-profile-edit/student-profile-edit.component.ts
+++ src/app/student-pages/student-profile-edit/student-profile-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-profile-edit',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentProfileEditComponent implements OnInit {
 
-  constructor() { }
+  user_name: string='';
+  user_roll: string='';
+  user_phone: string='';
+  user_session: string='';
+  user_email: string='';
+  user_password: string='';
+  user_confirm_password: string='';
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    var user = JSON.parse(localStorage.getItem("isLoggedIn"));
+    this.user_name = user.data.name;
+    this.user_roll = user.data.roll;
+    this.user_phone = user.data.phone;
+    this.user_email = user.data.email;
+    this.user_session = user.data.session;
+    this.user_password = user.data.password;
   }
+
+
 
 }
